ea84ff2a97048f0fcddf2a1a4f6b9afc33ed1e5c
paololeidi <leidi.paolo98@gmail.com>
2024-06-18 12:07:30 +0200

    Updated queries 1-3, refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,12 @@
 package org.example;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.configuration.MemorySize;
-import org.apache.flink.connector.file.sink.FileSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamUtils;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
-import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
@@ -33,7 +26,7 @@ public class Main {
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
         tableEnv.getConfig().getConfiguration().setString("table.exec.source.idle-timeout", "5000 ms");
 
-        KafkaSource<String> source = KafkaSource.<String>builder()
+        KafkaSource<String> stressSource = KafkaSource.<String>builder()
                 .setBootstrapServers("localhost:9092")
                 .setGroupId("my-group")
                 .setTopics("stress")
@@ -41,9 +34,9 @@ public class Main {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
-        DataStream<String> dataStream = env.fromSource(source, WatermarkStrategy.forMonotonousTimestamps(),"Kafka Source");
+        DataStream<String> stressStream = env.fromSource(stressSource, WatermarkStrategy.forMonotonousTimestamps(),"Kafka Source");
 
-        DataStream<Event> rowStream = dataStream.map(str -> {
+        DataStream<StressEvent> rowStressStream = stressStream.map(str -> {
             String[] values = str.split(",");
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
             LocalDateTime localDateTime = LocalDateTime.parse(values[0], formatter);
@@ -52,67 +45,113 @@ public class Main {
             int id = Integer.parseInt(values[1]);
             String status = values[2];
             int stress_level = Integer.parseInt(values[3]);
-            return new Event(id, status, stress_level, ts);
+            return new StressEvent(id, status, stress_level, ts);
         });
 
-
-        Table inputTable = tableEnv.fromDataStream(rowStream);
-        inputTable.printSchema();
-
-
-
-        Schema schema = Schema.newBuilder()
+        Schema stressSchema = Schema.newBuilder()
                 .columnByExpression("ts", "CAST(eventTime AS TIMESTAMP_LTZ(3))")
                 .columnByExpression("proc_time", "PROCTIME()")
                 .watermark("ts", "ts - INTERVAL '2' SECOND")
                 .build();
-        Table inputTable2 = tableEnv.fromDataStream(rowStream, schema);
-        inputTable2.printSchema();
-        tableEnv.createTemporaryView("InputTable", inputTable2);
+        Table stressInputTable = tableEnv.fromDataStream(rowStressStream, stressSchema);
+        stressInputTable.printSchema();
+        tableEnv.createTemporaryView("Stress", stressInputTable);
+
+        KafkaSource<String> weightSource = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setGroupId("my-group")
+                .setTopics("weight")
+                .setStartingOffsets(OffsetsInitializer.latest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        DataStream<String> weightStream = env.fromSource(weightSource, WatermarkStrategy.forMonotonousTimestamps(),"Kafka Source");
+
+        DataStream<WeightEvent> rowWeightStream = weightStream.map(str -> {
+            String[] values = str.split(",");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTime localDateTime = LocalDateTime.parse(values[0], formatter);
+            ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.of("Europe/Paris"));
+            Instant ts = zonedDateTime.toInstant();
+            int id = Integer.parseInt(values[1]);
+            double weight = Double.parseDouble(values[2]);
+            return new WeightEvent(id, weight, ts);
+        });
+
+        Schema weightSchema = Schema.newBuilder()
+                .columnByExpression("ts", "CAST(eventTime AS TIMESTAMP_LTZ(3))")
+                .columnByExpression("proc_time", "PROCTIME()")
+                .watermark("ts", "ts - INTERVAL '2' SECOND")
+                .build();
+        Table weightInputTable = tableEnv.fromDataStream(rowWeightStream, weightSchema);
+        weightInputTable.printSchema();
+        tableEnv.createTemporaryView("Weight", weightInputTable);
+
+
+
+
 
         ArrayList<Table> tableList = new ArrayList<>();
 
         Table table1 = tableEnv.sqlQuery("""
-                SELECT window_start, window_end, avg(stressLevel) as avg_stress
-                FROM table(TUMBLE(table InputTable, descriptor(ts), INTERVAL '10' seconds))
+                SELECT window_start, window_end, max(stressLevel) as max_stress
+                FROM table(TUMBLE(table Stress, descriptor(ts), INTERVAL '10' seconds))
                 GROUP BY window_start, window_end
                 """);
         tableList.add(table1);
 
         Table table2 = tableEnv.sqlQuery("""
-                SELECT window_start, window_end, avg(stressLevel) as avg_stress
-                FROM table(HOP(table InputTable, descriptor(ts), INTERVAL '5' seconds, INTERVAL '10' seconds))
+                SELECT window_start, window_end, max(stressLevel) as max_stress
+                FROM table(HOP(table Stress, descriptor(ts), INTERVAL '5' seconds, INTERVAL '10' seconds))
                 GROUP BY window_start, window_end
                 """);
         tableList.add(table2);
 
         Table table3 = tableEnv.sqlQuery("""
-                SELECT window_start, window_end,  avg(stressLevel) as avg_stress
-                FROM table(HOP(table InputTable, descriptor(ts), INTERVAL '1' seconds, INTERVAL '10' seconds))
+                SELECT window_start, window_end,  max(stressLevel) as max_stress
+                FROM table(HOP(table Stress, descriptor(ts), INTERVAL '1' seconds, INTERVAL '10' seconds))
                 GROUP BY window_start, window_end
                 """);
         tableList.add(table3);
 
         Table table4 = tableEnv.sqlQuery("""
                 SELECT window_start, window_end, id, max(stressLevel) as max_stress
-                FROM table(TUMBLE(table InputTable, descriptor(ts), INTERVAL '10' seconds))
+                FROM table(TUMBLE(table Stress, descriptor(ts), INTERVAL '10' seconds))
                 GROUP BY window_start, window_end, id
                 """);
         tableList.add(table4);
 
         Table table5 = tableEnv.sqlQuery("""
                 SELECT window_start, window_end, id, max(stressLevel) as max_stress
-                FROM table(HOP(table InputTable, descriptor(ts), INTERVAL '5' seconds, INTERVAL '10' seconds))
+                FROM table(HOP(table Stress, descriptor(ts), INTERVAL '5' seconds, INTERVAL '10' seconds))
                 GROUP BY window_start, window_end, id
                 """);
         tableList.add(table5);
 
         Table table6 = tableEnv.sqlQuery("""
                 SELECT window_start, window_end, id, max(stressLevel) as max_stress
-                FROM table(HOP(table InputTable, descriptor(ts), INTERVAL '1' seconds, INTERVAL '10' seconds))
+                FROM table(HOP(table Stress, descriptor(ts), INTERVAL '1' seconds, INTERVAL '10' seconds))
                 GROUP BY window_start, window_end, id
                 """);
         tableList.add(table6);
+
+        Table join = tableEnv.sqlQuery("""
+                SELECT CAST(s.ts as TIMESTAMP), s.id, s.status, s.stressLevel, CAST(w.ts as TIMESTAMP), w.weight
+                FROM Stress s, Weight w
+                WHERE s.id = w.id
+                AND w.ts BETWEEN s.ts AND s.ts + INTERVAL '10' SECOND
+                """);
+        tableList.add(join);
+
+        /*
+        Table join = tableEnv.sqlQuery("""
+                SELECT *
+                FROM table(HOP(table Stress, descriptor(ts), INTERVAL '1' seconds, INTERVAL '10' seconds))
+                GROUP BY window_start, window_end, id
+                """);
+        tableList.add(table6);
+
+         */
 
 /*
         for (int i = 1; i < tableList.size(); i++) {
@@ -127,13 +166,14 @@ public class Main {
  */
 
         // no ciclo for per questioni di timing con il risultato delle query
-        int i=0;
-        DataStream<Row> resultStream = tableEnv.toDataStream(tableList.get(i));
+        int i=3;
+        DataStream<Row> resultStream = tableEnv.toDataStream(tableList.get(i-1));
         DataStream<String> outputStream = resultStream.map(Row::toString);
         //resultStream.print();
         Iterator<String> myOutput = DataStreamUtils.collect(outputStream);
 
-        writeToFile("output"+String.valueOf(i+1)+".csv",myOutput);
+        writeToFile("Files/Output/output"+String.valueOf(i)+".csv",myOutput);
+        //writeToFile("Files/Output/join.csv",myOutput);
 
         /*final FileSink<String> sink = FileSink
                 .forRowFormat(new Path("output"), new SimpleStringEncoder<String>("UTF-8"))
@@ -180,7 +220,7 @@ public class Main {
         return o;
     }
 
-    public static class Event {
+    public static class StressEvent {
         public Integer id;
         public String status;
         public Integer stressLevel;
@@ -188,13 +228,29 @@ public class Main {
         public Instant eventTime;
 
         // default constructor for DataStream API
-        public Event() {}
+        public StressEvent() {}
 
         // fully assigning constructor for Table API
-        public Event(Integer id, String status, Integer stressLevel, Instant eventTime){
+        public StressEvent(Integer id, String status, Integer stressLevel, Instant eventTime){
             this.id = id;
             this.status = status;
             this.stressLevel = stressLevel;
+            this.eventTime = eventTime;
+        }
+    }
+
+    public static class WeightEvent {
+        public Integer id;
+        public Double weight;
+        public Instant eventTime;
+
+        // default constructor for DataStream API
+        public WeightEvent() {}
+
+        // fully assigning constructor for Table API
+        public WeightEvent(Integer id, Double weight, Instant eventTime){
+            this.id = id;
+            this.weight = weight;
             this.eventTime = eventTime;
         }
     }
